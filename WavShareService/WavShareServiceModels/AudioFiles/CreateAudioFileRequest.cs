6761be47f36d7b39b70e9f5809d1815a5d34b0bf
rlabausa <rubylabausa@gmail.com>
2022-09-11 13:07:13 -0600

    Add data annotations and default constructor

--- WavShareService/WavShareServiceModels/AudioFiles/CreateAudioFileRequest.cs
+++ WavShareService/WavShareServiceModels/AudioFiles/CreateAudioFileRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,20 @@ namespace WavShareServiceModels.AudioFiles
 {
     public class CreateAudioFileRequest
     {
+        [Required]
         public string AudioFileName { get; set; }
+
+        [Required]
         public string EncodedAudio { get; set; }
+        [Required]
         public string UploadedBy { get; set; }
+
+        public CreateAudioFileRequest(string audioFileName, string encodedAudio, string uploadedBy)
+        {
+            AudioFileName = audioFileName;
+            EncodedAudio = encodedAudio;
+            UploadedBy = uploadedBy;
+
+        }
     }
 }

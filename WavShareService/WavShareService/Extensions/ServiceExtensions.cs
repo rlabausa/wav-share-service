490e003166ead2c705fc6409ee2972f4778160f0
rlabausa <rubylabausa@gmail.com>
2022-09-17 15:23:58 -0600

    Clean up Program.cs

--- WavShareService/WavShareService/Extensions/ServiceExtensions.cs
+++ WavShareService/WavShareService/Extensions/ServiceExtensions.cs
@@ -1,10 +1,70 @@
 ﻿using WavShareServiceBLL;
 using WavShareServiceDAL;
+using WavShareServiceModels.ApiResponses;
 
 namespace WavShareService.Extensions
 {
     public static class ServiceExtensions
     {
+        /// <summary>
+        /// Add required controller configurations for the application.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns><see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection AddConfiguredControllers(this IServiceCollection services)
+        {
+            services.AddControllers()
+                .ConfigureApiBehaviorOptions(opts =>
+                {
+                    opts.InvalidModelStateResponseFactory = actionContext =>
+                    {
+                        var modelState = actionContext.ModelState;
+                        return new ApiValidationErrorResult(modelState);
+                    };
+                });
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add required logging configurations for the application.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns><see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection AddConfiguredLogging(this IServiceCollection services, IConfiguration? configuration = null)
+        {
+            services.AddLogging((logging) =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+                logging.AddDebug();
+
+                if (configuration != null)
+                {
+                    logging.AddConfiguration(configuration);
+                }
+            });
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add required routing configurations for the application.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns><see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+        public static IServiceCollection AddConfiguredRouting(this IServiceCollection services)
+        {
+            services.AddRouting(opts =>
+            {
+                opts.LowercaseUrls = true;
+            });
+
+            return services;
+        }
+
         /// <summary>
         /// Add all required transient services for the application.
         /// </summary>
--- WavShareService/WavShareService/Program.cs
+++ WavShareService/WavShareService/Program.cs
@@ -8,29 +8,9 @@ using WavShareServiceModels.ApiResponses;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddLogging((logging) =>
-{
-    logging.ClearProviders();
-    logging.AddConsole();
-    logging.AddDebug();
-    logging.AddConfiguration(builder.Configuration);
-
-});
-
-builder.Services.AddRouting(opts =>
-{
-    opts.LowercaseUrls = true;
-});
-
-builder.Services.AddControllers()
-                .ConfigureApiBehaviorOptions(opts =>
-                {
-                    opts.InvalidModelStateResponseFactory = actionContext =>
-                    {
-                        var modelState = actionContext.ModelState;
-                        return new ApiValidationErrorResult(modelState);
-                    };
-                });
+builder.Services.AddConfiguredLogging(builder.Configuration);
+builder.Services.AddConfiguredRouting();
+builder.Services.AddConfiguredControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddConfiguredSwaggerGen();
 builder.Services.AddTransientServices();

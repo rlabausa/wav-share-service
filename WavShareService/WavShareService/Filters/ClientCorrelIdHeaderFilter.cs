df87903cd2708ccb128e02531509fd9b04431674
rlabausa <rubylabausa@gmail.com>
2022-09-22 19:43:49 -0600

    Add ClientCorrelId OperationFilter

---
+++ WavShareService/WavShareService/Filters/ClientCorrelIdHeaderFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Writers;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using WavShareServiceModels.Constants;
+
+namespace WavShareService.Filters
+{
+    public class ClientCorrelIdHeaderFilter: IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+            {
+                operation.Parameters = new List<OpenApiParameter>();
+            }
+
+            operation.Parameters.Insert(0, new OpenApiParameter()
+            {   Name = Header.ClientCorrelId,
+                In = ParameterLocation.Header,
+                Required = true,
+                Schema = new OpenApiSchema()
+                {
+                    Type = "string",
+                    Example = new OpenApiString("generate"), 
+                    Default = new OpenApiString("generate")
+                },
+                Description = "Use 'generate' if not passing in a unique correlation id",
+                AllowEmptyValue = false
+            });
+
+
+        }
+    }
+}

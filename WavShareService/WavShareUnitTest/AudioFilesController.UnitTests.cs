3fd8ae8866d28dceee487405a5d332d9b00cda8a
rlabausa <rubylabausa@gmail.com>
2022-09-25 22:16:35 -0600

    Add test case for POST audio file

--- WavShareService/WavShareUnitTest/AudioFilesController.UnitTests.cs
+++ WavShareService/WavShareUnitTest/AudioFilesController.UnitTests.cs
@@ -27,20 +27,29 @@ namespace WavShareUnitTest
             Assert.NotNull(result);
         }
 
+        [Fact]
+        public async Task Post_Returns_IActionResult()
         {
             // Arrange
             var mockLogger = new Mock<ILogger<AudioFilesController>>();
             var mockAudioFileBLL = new Mock<IAudioFileBLL>();
             var audioFileController = new AudioFilesController(mockAudioFileBLL.Object, mockLogger.Object);
 
-            var request = new GetAudioFilesRequest();
+            var request = new CreateAudioFileRequest(
+                    "hello_there.wav",
+                    "ZmFrZV9iYXNlNjRfZW5jb2RlZF9zdHJpbmc=",
+                    "General Kenobi"
+                );
+
+            mockAudioFileBLL.Setup(x => x.CreateAudioFile(request))
+                .ReturnsAsync(1);
 
 
             // Act
-            var result = await audioFileController.Get(request);
+            var result = await audioFileController.Post(request);
 
             // Assert
-            Assert.IsType<ActionResult<IEnumerable<GetAudioFilesResponse>>>(result);
+            Assert.IsType<CreatedAtActionResult>(result);
             Assert.NotNull(result);
         }
     }

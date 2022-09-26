8038c0a973e71b24be9a57c55d7268088a926457
rlabausa <rubylabausa@gmail.com>
2022-09-25 22:15:45 -0600

    Rename test case

--- WavShareService/WavShareUnitTest/AudioFilesController.UnitTests.cs
+++ WavShareService/WavShareUnitTest/AudioFilesController.UnitTests.cs
@@ -9,7 +9,24 @@ namespace WavShareUnitTest
     public class AudioFilesControllerTest
     {
         [Fact]
-        public async Task AudioFilesController_Returns_GetAudioFilesResponse()
+        public async Task Get_Returns_GetAudioFilesResponse()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<AudioFilesController>>();
+            var mockAudioFileBLL = new Mock<IAudioFileBLL>();
+            var audioFileController = new AudioFilesController(mockAudioFileBLL.Object, mockLogger.Object);
+
+            var request = new GetAudioFilesRequest();
+
+
+            // Act
+            var result = await audioFileController.Get(request);
+
+            // Assert
+            Assert.IsType<ActionResult<IEnumerable<GetAudioFilesResponse>>>(result);
+            Assert.NotNull(result);
+        }
+
         {
             // Arrange
             var mockLogger = new Mock<ILogger<AudioFilesController>>();

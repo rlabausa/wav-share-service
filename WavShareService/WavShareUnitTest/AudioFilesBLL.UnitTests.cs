086ab646c10800b91c57ac4efbf041aedea615ea
rlabausa <rubylabausa@gmail.com>
2022-09-26 22:39:56 -0600

    Add BLL unit test file

---
+++ WavShareService/WavShareUnitTest/AudioFilesBLL.UnitTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WavShareService.Controllers;
+using WavShareServiceBLL;
+using WavShareServiceDAL;
+using WavShareServiceModels.AudioFiles;
+
+namespace WavShareUnitTest
+{
+    public class AudioFilesBLLTest
+    {
+        [Fact]
+        public async Task GetAudioFiles_Returns_GetAudioFilesResponse()
+        {
+            var mockLogger = new Mock<ILogger<AudioFileBLL>>();
+            var mockAudioFileAdapter = new Mock<IAudioFileAdapter>();
+            var audioFileBLL = new AudioFileBLL(mockAudioFileAdapter.Object);
+
+            var mockRequest = new GetAudioFilesRequest();
+            var mockResponse = new GetAudioFilesResponse()
+            {
+                AudioFiles = new List<AudioFile>()
+                {
+                    new AudioFile(1, "hello.wav", "aGVsbG8gd29ybGQ=", "ruby", DateTime.Now),
+                    new AudioFile(2, "goodbye.wav", "Z29vZGJ5ZSB3b3JsZA==", "john", DateTime.Now)
+                }
+            };
+
+            mockAudioFileAdapter
+                .Setup(x => x.GetAudioFiles(mockRequest))
+                .ReturnsAsync(mockResponse);
+
+            var result = await audioFileBLL.GetAudioFiles(mockRequest);
+            
+            Assert.NotNull(result);
+
+        }
+    }
+}

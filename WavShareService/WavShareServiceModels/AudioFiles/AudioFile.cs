2abfd20c5808878bbab06a645b724714486ed41d
rlabausa <rubylabausa@gmail.com>
2022-10-01 12:52:36 -0600

    Derive AudioFile from AudioFileDetails class

--- WavShareService/WavShareServiceModels/AudioFiles/AudioFile.cs
+++ WavShareService/WavShareServiceModels/AudioFiles/AudioFile.cs
@@ -7,39 +7,20 @@ using System.Threading.Tasks;
 
 namespace WavShareServiceModels.AudioFiles
 {
-    public class AudioFile
+    public class AudioFile : AudioFileDetails
     {
-        public AudioFile()
+        public AudioFile() : base()
         {
-            AudioFileId = -1;
-            AudioFileName = string.Empty;
             EncodedAudio = string.Empty;
-            UploadedBy = string.Empty;
-            UploadDate = DateTime.UnixEpoch;
         }
 
         public AudioFile(int audioFileId, string audioFileName, string encodedAudio, string uploadedBy, DateTime uploadDate)
+            : base(audioFileId, audioFileName, uploadedBy, uploadDate)
         {
-            AudioFileId = audioFileId;
-            AudioFileName = audioFileName;
             EncodedAudio = encodedAudio;
-            UploadedBy = uploadedBy;
-            UploadDate = uploadDate;
         }
-
-        [JsonPropertyName("audioFileId")]
-        public int AudioFileId { get; set; }
-
-        [JsonPropertyName("audioFileName")]
-        public string AudioFileName { get; set; }
 
         [JsonPropertyName("encodedAudio")]
         public string EncodedAudio { get; set; }
-
-        [JsonPropertyName("uploadedBy")]
-        public string UploadedBy { get; set; }
-
-        [JsonPropertyName("uploadDate")]
-        public DateTime UploadDate { get; set; }
     }
 }
